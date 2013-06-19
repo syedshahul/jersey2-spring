4bf92ba228fd54623d94ef6eca2685450dba76c9
syedshahul <syedshahul2002@gmail.com>
2013-06-19 16:31:08 +0530

    WIP Jersey2 and Spring integeration

---
+++ service-layer/src/main/java/org/jersey/sample/service/impl/ManagerImpl.java
@@ -0,0 +1,12 @@
+package org.jersey.sample.service.impl;
+
+import org.jersey.sample.service.Manager;
+
+/**
+ * @author: Syed Shahul
+ */
+public class ManagerImpl implements Manager {
+ public String getIt(){
+   return "Got it- Manager ";
+ }
+}
